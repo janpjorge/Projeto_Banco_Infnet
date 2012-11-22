6e7a74786bbaed63b0fbb6f081205bb09359c50a
Rafael Santos <santos.rafaelcarlos@gmail.com>
2012-11-22 10:57:26 -0200

    Mais Testes
    Terminando testes da conta especial e atualizando o diagrama de classes

--- Infnet.Proj.SistBancario/Infnet.Proj.SistBancario.Teste/ContaEspecialTest.cs
+++ Infnet.Proj.SistBancario/Infnet.Proj.SistBancario.Teste/ContaEspecialTest.cs
@@ -13,13 +13,96 @@ namespace Infnet.Proj.SistBancario.Teste
 {
     [TestFixture]
     public class ContaEspecialTest
-    {
-        //public double LimiteDisponivel 
-        //public double Limite 
-        //public double SaldoSimples       
-        //public double LimiteUtilizado
-        //public override double Saldo
-        
+    {        
+        [TestCase]
+        public void LimiteTest()
+        {
+            Agencia ag = Fabrica.CriaAgencia();
+
+            PessoaJuridica cliente = (PessoaJuridica)Fabrica.CriaCliente(false);
+
+            int numConta = ag.CriarConta(new Cliente[] { cliente });
+
+            ContaEspecial conta = (ContaEspecial)ag.RetornaConta(numConta);
+
+            double limiteEsperado = cliente.Receita * 0.75;
+                        
+            Assert.AreEqual(limiteEsperado, conta.Limite);
+        }
+
+        [TestCase]
+        public void LimiteUtilizadoTest()
+        {
+            Agencia ag = Fabrica.CriaAgencia();
+
+            PessoaJuridica cliente = (PessoaJuridica)Fabrica.CriaCliente(false);
+
+            int numConta = ag.CriarConta(new Cliente[] { cliente });
+
+            ContaEspecial conta = (ContaEspecial)ag.RetornaConta(numConta);
+
+            double esperado = 1000;
+
+            conta.DebitaValor(1000);
+
+            Assert.AreEqual(esperado, conta.LimiteUtilizado);
+        }
+
+
+        [TestCase]
+        public void LimiteDisponivelTest()
+        {
+            Agencia ag = Fabrica.CriaAgencia();
+
+            PessoaJuridica cliente = (PessoaJuridica)Fabrica.CriaCliente(false);
+
+            int numConta = ag.CriarConta(new Cliente[] { cliente });
+
+            ContaEspecial conta = (ContaEspecial)ag.RetornaConta(numConta);
+
+            double esperado = (cliente.Receita * 0.75) - 1000;
+
+            conta.DebitaValor(1000);
+
+            Assert.AreEqual(esperado, conta.LimiteDisponivel);
+        }
+
+        [TestCase]
+        public void SaldoTest()
+        {
+            Agencia ag = Fabrica.CriaAgencia();
+
+            PessoaJuridica cliente = (PessoaJuridica)Fabrica.CriaCliente(false);
+
+            int numConta = ag.CriarConta(new Cliente[] { cliente });
+
+            ContaEspecial conta = (ContaEspecial)ag.RetornaConta(numConta);
+
+            conta.CreditaValor(1000);
+
+            double saldoEsperado = (cliente.Receita * 0.75) + 1000;
+
+            Assert.AreEqual(saldoEsperado, conta.Saldo);
+        }
+
+        [TestCase]
+        public void SaldoSimplesTest()
+        {
+            Agencia ag = Fabrica.CriaAgencia();
+
+            PessoaJuridica cliente = (PessoaJuridica)Fabrica.CriaCliente(false);
+
+            int numConta = ag.CriarConta(new Cliente[] { cliente });
+
+            ContaEspecial conta = (ContaEspecial)ag.RetornaConta(numConta);
+
+            conta.CreditaValor(1000);
+
+            double saldoEsperado = 1000;
+
+            Assert.AreEqual(saldoEsperado, conta.SaldoSimples);
+        }
+
         [TestCase]
         public void DebitaValorTest()
         {
